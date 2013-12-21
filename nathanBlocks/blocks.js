b2937448f4ccded6df7a0695b29221e3c7fec4dd
redstranger7 <jstrange148@gmail.com>
2013-12-20 23:52:45 -0500

    Append attempt
    Creates a buttonDiv ID in the css and attempts to call it with JQuery as
    part of the createTheButton function

--- nathanBlocks/blocks.js
+++ nathanBlocks/blocks.js
@@ -1,6 +1,9 @@
+
 function createTheButton() {
 
 	// Do something!!!
 	alert("hi there");
-	
+
+	// This should 'append' buttonDiv as the end, as the last child of the body.
+	$("body").append('<div id = "buttonDiv"></div>');	            
 }
