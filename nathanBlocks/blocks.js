e6be0fe381cbe3d5d5ea0a28046645e2f154c593
redstranger7 <jstrange148@gmail.com>
2013-12-26 23:46:56 -0500

    My powers grow!!! Well, sort of....
    A couple changes:
    I removed the closing </input> tags, because input tags are
    self-closing.  I also removed the id property and replaced it with name
    properties, to try and make it fit better with what CodeCademy has had
    me doing.
    On the JS file, I added some variables to store the values from the text
    inputs and then functions to pull them.
    Within the createTheButton function, I call on those functions, and
    nothing after that point in the function seems to work.

--- nathanBlocks/blocks.js
+++ nathanBlocks/blocks.js
@@ -1,8 +1,35 @@
 
+//Declare variables to hold values for buttonDiv property values
+var divHeight = 0
+var divWidth = 0
+var divColor = 0
+
+//create functions to pull data from input fields
+var getHeight = function()	{
+	return ($('input[name = "blockHeight"]').val());
+	};
+
+var getWidth = function()	{
+	return ($('input[name = "blockWidth"]').val());
+	};
+
 function createTheButton() {
 
 	// Do something!!!
-	alert("hi there");
+	alert("Hi there");
+
+	//Set values of variables using functions above
+	divHeight = getHeight();
+	divWidth = getWidth();
+	
+	//Test that functions worked
+	alert(divHeight);
+	alert(divWidth);
+
+	//Assign buttonDiv the value of the variables
+	$('buttonDiv').css('height',divHeight);
+	$('buttonDiv').css('width',divWidth);
+
 
 	// This should 'append' buttonDiv as the end, as the last child of the body.
 	$("body").append('<div id = "buttonDiv"></div>');	            
