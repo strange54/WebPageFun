7fe0a0577501ce5e1a274f0b20470fc0ba03dea6
strange54 <strange54@gmail.com>
2013-12-30 11:02:55 -0600

    Each block's ID is now unique
    What the subject says.

--- nathanBlocks/blocks.js
+++ nathanBlocks/blocks.js
@@ -1,8 +1,9 @@
 
 //Declare variables to hold values for buttonDiv property values
-var divHeight = 0
-var divWidth = 0
-var divColor = 0
+var divHeight = 0;
+var divWidth = 0;
+var divColor = 0;
+var idCounter = 0;
 
 //create functions to pull data from input fields
 var getHeight = function()	{
@@ -23,14 +24,21 @@ function createTheButton() {
 	divWidth = getWidth()+'px';
 	
 	//Test that functions worked
-	alert(divHeight);
-	alert(divWidth);
 
+	//alert(divHeight);
+	//alert(divWidth);
+
+	var blockId = "buttonDiv" + String(idCounter);
+	var block = "<div id = \"" + blockId + "\"></div>";
+	idCounter = idCounter + 1;
+	
 	// This should 'append' buttonDiv as the end, as the last child of the body.
-	$("body").append('<div id = "buttonDiv"></div>');	  
+	$("body").append(block);
 	$("body").append('<br />');	
 	
     //Assign buttonDiv the value of the variables
-	$('#buttonDiv').css('height',divHeight);
-	$('#buttonDiv').css('width',divWidth);
+	$('#'+blockId).css('height',divHeight);
+	$('#'+blockId).css('width',divWidth);
+	$('#'+blockId).css('background-color','red');
+
 }
